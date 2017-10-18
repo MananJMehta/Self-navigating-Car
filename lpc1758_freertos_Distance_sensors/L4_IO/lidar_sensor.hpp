8f4d7ff34480d6816994442fbb723b563c8b5496
Hugo <hugo.quiroz92@gmail.com>
2017-10-17 22:59:25 -0700

    added structs to contain data packets

--- lpc1758_freertos_Distance_sensors/L4_IO/lidar_sensor.hpp
+++ lpc1758_freertos_Distance_sensors/L4_IO/lidar_sensor.hpp
@@ -22,6 +22,8 @@ class Lidar_Sensor : public SingletonTemplate<Lidar_Sensor>
 {
     public:
 
+
+
         typedef enum {
             lidar_stop_scan = 0x25,
             lidar_reset_core = 0x40,
@@ -33,30 +35,56 @@ class Lidar_Sensor : public SingletonTemplate<Lidar_Sensor>
             lidar_get_sample_rate = 0x59,
         } lidar_cmd_t;
 
+
+
         //Struct that contains the 5 bytes of data coming from lidar scan command
         typedef struct {
             uint8_t quality; //the quality of the data reading
             uint8_t angle; //the angle of the data point
             uint8_t distance_1; //first byte describing distance
             uint8_t distance_2; //second byte describing distance
-        } __attribute__((__packed__)) lidar_data_t;
+        } __attribute__((__packed__)) scan_data_packet_t;
+
+        //Struct that contains the 5 bytes of data coming from lidar scan command
+        typedef struct {
+            uint8_t tstandard1; //the quality of the data reading
+            uint8_t tstandard2; //the angle of the data point
+            uint8_t texpress1; //first byte describing distance
+            uint8_t texpress2; //second byte describing distance
+        } __attribute__((__packed__)) sample_rate_packet_t;
+
+        //Struct that contains the 5 bytes of data coming from lidar scan command
+        typedef struct {
+            uint8_t model; //the quality of the data reading
+            uint8_t firmware_minor; //the angle of the data point
+            uint8_t firmware_major; //first byte describing distance
+            uint8_t hardware; //second byte describing distance
+            uint8_t serial_number [16];
+        } __attribute__((__packed__)) info_data_packet_t;
+
+        //Struct that contains the 5 bytes of data coming from lidar scan command
+        typedef struct {
+            uint8_t status; //the quality of the data reading
+            uint8_t error_code1; //the angle of the data point
+            uint8_t error_code2; //first byte describing distance
+        } __attribute__((__packed__)) health_data_packet_t;
 
 
         bool init(); ///< Initializes this device, @returns true if successful
 
         bool stop_scan();//this is a unidriectional command we wil return true for success
         bool reset_core();//unidirectional, send true for succesful reset
-        bool start_scan();//pass a data structure to this function, return true for succces
+        bool start_scan(scan_data_packet_t *lidar_data);//pass a data structure to this function, return true for succces
         bool start_express_scan();//pass data structure, return true
         bool start_force_scan();
-        void get_info();
-        void get_health();
-        void get_sample_rate();
-        void set_motor_speed(uint8_t PWM, uint8_t frequency ,uint8_t duty_cycle);
+        void get_info(info_data_packet_t *lidar_info);
+        void get_health(health_data_packet_t *health_data);
+        void get_sample_rate(sample_rate_packet_t *sample_rate);
+        void set_motor_speed(uint8_t RPM);
 
     private:
         bool send_lidar_command(lidar_cmd_t lidar_cmd);//we will send an enum data type
-        void receive_lidar_command(lidar_cmd_t lidar_cmd);
+        void receive_lidar_data(lidar_cmd_t lidar_cmd);
 
         Lidar_Sensor() { }  ///< Private constructor of this Singleton class
         friend class SingletonTemplate<Lidar_Sensor>;  ///< Friend class used for Singleton Template
--- lpc1758_freertos_Distance_sensors/L4_IO/src/lidar_sensor.cpp
+++ lpc1758_freertos_Distance_sensors/L4_IO/src/lidar_sensor.cpp
@@ -7,55 +7,95 @@
 
 bool Lidar_Sensor::init()
 {
+    //set frequency
+    //set_PWM
+    //set duty_cycle
+
     u2.init(115200, 360, 360);
     return true;
 }
 
+/*
+ *
+ * This command will not return anything but it will stop the
+Lidar Sensor, you should wait 1ms before sending another command
+also we should send command to see status of lidar to make sure lidar
+is stopped, maybe get sample rate
+
+*/
 bool Lidar_Sensor::stop_scan()
 {
     if(send_lidar_command(lidar_stop_scan)) return true;
     else return false;
 }
+
+
+/*
+ *
+ * Sending a reset command will revert the Lidar to a similar state as if
+ * the sensor has been booted up, wait about 2 ms before sending next command
+ * we should send command to check status of lidar to ensure lidar is stopped
+ * maybe get sample rate
+ *
+ *
+ * */
+
 bool Lidar_Sensor::reset_core()
 {
+    if(send_lidar_command(lidar_reset_core)) return true;
     return 0;
 }
 
-bool Lidar_Sensor::start_scan()
+
+/*
+ *
+ *
+ *
+ */
+
+bool Lidar_Sensor::start_scan(scan_data_packet_t *lidar_data)
 {
+    if(send_lidar_command(lidar_start_scan)) return true;
     return 0;
 }
 
 bool Lidar_Sensor::start_express_scan()
 {
+    if(send_lidar_command(lidar_start_exp_scan)) return true;
     return 0;
 }
 
 bool Lidar_Sensor::start_force_scan()
 {
+    if(send_lidar_command(lidar_start_force_scan)) return true;
     return 0;
 }
 
-void Lidar_Sensor::get_info()
+void Lidar_Sensor::get_info(info_data_packet_t *lidar_info)
 {
+
+    send_lidar_command(lidar_get_info); //do nothing
+
 
 }
 
 
 //this should return something or put something somewhere maybe a struct??
-void Lidar_Sensor::get_health()
+void Lidar_Sensor::get_health(health_data_packet_t *health_data)
 {
     send_lidar_command(lidar_get_health);
 }
 
-void Lidar_Sensor::get_sample_rate()
+void Lidar_Sensor::get_sample_rate(sample_rate_packet_t *sample_rate)
 {
-
+    send_lidar_command(lidar_get_sample_rate);
 }
 
-void Lidar_Sensor::set_motor_speed(uint8_t PWM, uint8_t frequency, uint8_t duty_cycle)
+void Lidar_Sensor::set_motor_speed(uint8_t RPM)
 {
+    sample_rate_packet_t sample_rate;
 
+    get_sample_rate(&sample_rate);
 }
 
 bool Lidar_Sensor::send_lidar_command(lidar_cmd_t lidar_cmd)
@@ -65,7 +105,7 @@ bool Lidar_Sensor::send_lidar_command(lidar_cmd_t lidar_cmd)
 }
 
 //put received thingies in a queue maybe??
-void Lidar_Sensor::receive_lidar_command(lidar_cmd_t lidar_cmd)
+void Lidar_Sensor::receive_lidar_data(lidar_cmd_t lidar_cmd)
 {
     //u2.gets(pBuff, maxLen, timeout);
 }
