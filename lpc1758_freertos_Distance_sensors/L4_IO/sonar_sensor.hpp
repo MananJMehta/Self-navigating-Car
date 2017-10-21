d398d13c344ca52e1b93f9fe2da9d125df80b4d3
Pushpender Singh <psingh0701@live.com>
2017-10-21 14:03:03 -0700

    added Sonar hpp file

---
+++ lpc1758_freertos_Distance_sensors/L4_IO/sonar_sensor.hpp
@@ -0,0 +1,11 @@
+#ifndef SONAR_SENSOR_HPP_
+#define SONAR_SENSOR_HPP_
+#include <stdint.h>
+
+/*
+ * Sonar Sensor class used to get sonar reading from the MaxSonar board using the PWM
+ * add more details here.....
+ * 
+ * @ingroup BoardIO
+ */
+
