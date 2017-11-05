93e5fe643debd3b5130bc16098c1e91b20fec721
VenkatRajaIyer <venkatraja1234@gmail.com>
2017-11-04 20:28:12 -0700

    Changes in LCD module to extract data from CANBUS

--- lpc1758_freertos_LCD_motor/L5_Application/periodic_scheduler/lcd.cpp
+++ lpc1758_freertos_LCD_motor/L5_Application/periodic_scheduler/lcd.cpp
@@ -14,7 +14,7 @@ enum lcd_led{Sensor, Motor, Comm, Geo};
 
 int get_random_int(int max_val)
 {
-    srand((unsigned) time(0)                        );
+    srand((unsigned) time(0));
     int random_integer = rand();
     int value = random_integer % max_val;
     return value;
@@ -23,13 +23,13 @@ int get_random_int(int max_val)
 void display_speedometer(char random_speed)
 {
     Uart2& u2 = Uart2::getInstance();
-    u2.putChar(0x01);   //Write Object
-    u2.putChar(0x10);   //Object ID for speedometer
-    u2.putChar(0x00);
-    u2.putChar(0x00);   //MSB
-    u2.putChar(random_speed); //LSB
+    u2.putChar(0x01, portMAX_DELAY);   //Write Object
+    u2.putChar(0x10, portMAX_DELAY);   //Object ID for speedometer
+    u2.putChar(0x00, portMAX_DELAY);
+    u2.putChar(0x00, portMAX_DELAY);   //MSB
+    u2.putChar(random_speed, portMAX_DELAY); //LSB
     char checkSum = 0x01^0x10^0x00^0x00^ random_speed;
-    u2.putChar(checkSum);
+    u2.putChar(checkSum, portMAX_DELAY);
     ack();
 }
 
@@ -37,13 +37,13 @@ void display_bus_reset()
 {
     static char bus_reset_count;
     Uart2& u2 = Uart2::getInstance();
-    u2.putChar(0x01);   //Write Object
-    u2.putChar(0x09);   //Object ID for bus_reset
-    u2.putChar(0x00);
-    u2.putChar(0x00);   //MSB
-    u2.putChar(++bus_reset_count); //LSB
+    u2.putChar(0x01, portMAX_DELAY);   //Write Object
+    u2.putChar(0x09, portMAX_DELAY);   //Object ID for bus_reset
+    u2.putChar(0x00, portMAX_DELAY);
+    u2.putChar(0x00, portMAX_DELAY);   //MSB
+    u2.putChar(++bus_reset_count, portMAX_DELAY); //LSB
     char checkSum = 0x01^0x09^0x00^0x00^ bus_reset_count;
-    u2.putChar(checkSum);
+    u2.putChar(checkSum, portMAX_DELAY);
     ack();
 }
 
@@ -64,26 +64,26 @@ void display_lidar_spectrum(char pos, char val)
 void display_lcd_led(char led_num, char state)
 {
     Uart2& u2 = Uart2::getInstance();
-    u2.putChar(0x01);   //Write Object
-    u2.putChar(0x0E);   //Object ID for bus_reset
-    u2.putChar(led_num);
-    u2.putChar(0x00);
-    u2.putChar(state);  //LSB
+    u2.putChar(0x01, portMAX_DELAY);   //Write Object
+    u2.putChar(0x0E, portMAX_DELAY);   //Object ID for bus_reset
+    u2.putChar(led_num, portMAX_DELAY);
+    u2.putChar(0x00, portMAX_DELAY);
+    u2.putChar(state, portMAX_DELAY);  //LSB
     char checkSum = 0x01^0x0E^led_num^0x00^state;
-    u2.putChar(checkSum);
+    u2.putChar(checkSum, portMAX_DELAY);
     ack();
 }
 
 void display_LCD_large_led(char led_num, char state)
 {
     Uart2& u2 = Uart2::getInstance();
-    u2.putChar(0x01);   //Write Object
-    u2.putChar(0x13);   //Object ID for bus_reset
-    u2.putChar(led_num);
-    u2.putChar(0x00);
-    u2.putChar(state); //LSB
+    u2.putChar(0x01, portMAX_DELAY);   //Write Object
+    u2.putChar(0x13, portMAX_DELAY);   //Object ID for bus_reset
+    u2.putChar(led_num, portMAX_DELAY);
+    u2.putChar(0x00, portMAX_DELAY);
+    u2.putChar(state, portMAX_DELAY); //LSB
     char checkSum = 0x01^0x13^led_num^0x00^state;
-    u2.putChar(checkSum);
+    u2.putChar(checkSum, portMAX_DELAY);
     ack();
 }
 
@@ -98,13 +98,13 @@ void display_lcd_numbers(char display_num, uint16_t value)
     uint8_t lsb, msb;
     convert16_to_hex(value, &lsb, &msb);
     Uart2& u2 = Uart2::getInstance();
-    u2.putChar(0x01);   //Write Object
-    u2.putChar(0x0F);   //Object ID for bus_reset
-    u2.putChar(display_num);
-    u2.putChar(msb);
-    u2.putChar(lsb);    //LSB
+    u2.putChar(0x01, portMAX_DELAY);   //Write Object
+    u2.putChar(0x0F, portMAX_DELAY);   //Object ID for bus_reset
+    u2.putChar(display_num, portMAX_DELAY);
+    u2.putChar(msb, portMAX_DELAY);
+    u2.putChar(lsb, portMAX_DELAY);    //LSB
     char checkSum = 0x01^0x0F^display_num^msb^lsb;
-    u2.putChar(checkSum);
+    u2.putChar(checkSum, portMAX_DELAY);
     ack();
 }
 
@@ -113,13 +113,13 @@ void display_lcd_bar(char display_num, uint16_t value)
     uint8_t lsb, msb;
     convert16_to_hex(value, &lsb, &msb);
     Uart2& u2 = Uart2::getInstance();
-    u2.putChar(0x01);   //Write Object
-    u2.putChar(0x0B);   //Object ID for bus_reset
-    u2.putChar(display_num);
-    u2.putChar(msb);
-    u2.putChar(lsb);    //LSB
+    u2.putChar(0x01, portMAX_DELAY);   //Write Object
+    u2.putChar(0x0B, portMAX_DELAY);   //Object ID for bus_reset
+    u2.putChar(display_num, portMAX_DELAY);
+    u2.putChar(msb, portMAX_DELAY);
+    u2.putChar(lsb, portMAX_DELAY);    //LSB
     char checkSum = 0x01^0x0B^display_num^msb^lsb;
-    u2.putChar(checkSum);
+    u2.putChar(checkSum, portMAX_DELAY);
     ack();
 }
 
@@ -134,12 +134,12 @@ char check_form()
     char test[1] = {'\0'};
     char *input = hello;
     char *form_value = test;
-    u2.getChar(input);
-    u2.getChar(++input);
-    u2.getChar(++input);
-    u2.getChar(++input);
-    u2.getChar(form_value);
-    u2.getChar(++input);
+    u2.getChar(input, portMAX_DELAY);
+    u2.getChar(++input, portMAX_DELAY);
+    u2.getChar(++input, portMAX_DELAY);
+    u2.getChar(++input, portMAX_DELAY);
+    u2.getChar(form_value, portMAX_DELAY);
+    u2.getChar(++input, portMAX_DELAY);
     printf("%d\n", *form_value);
     return test[0];
 }
@@ -150,7 +150,7 @@ void ack()
     Uart2& u2 = Uart2::getInstance();
        char test[1] = {'\0'};
         char *ack_val = test;
-        u2.getChar(ack_val, 100);
+        u2.getChar(ack_val, portMAX_DELAY);
 //        printf("%c", ack_val);
 }
 
--- lpc1758_freertos_LCD_motor/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ lpc1758_freertos_LCD_motor/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -51,12 +51,20 @@ const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
  */
 const uint32_t PERIOD_MONITOR_TASK_STACK_SIZE_BYTES = (512 * 3);
 
-enum lcd_led{Sensor, Motor, Comm, Geo};
+//enum lcd_led{Sensor, Motor, Comm, Geo};
 enum lcd_digits{Miles_covered, Miles_remaining, Dest_lat, Dest_long, Current_lat, Current_long};
 enum lcd_health{System, Battery, Degree0, Degree20, Degree40, Degree60, Degree80, Degree_neg20, Degree_neg40, Degree_neg60, Degree_neg80};
 enum lcd_status{Off, On};
 enum lcd_ultrasound{Ultrasound_left, Ultrasound_front, Ultrasound_right};
+enum lcd_form{Main_page, Sensor_page, Distance_page, GPS_page};
 
+/*
+ * Global variables to pass data for in 1 Hz scheduler from 10 Hz scheduler
+ */
+uint8_t speed, us_front, us_left, us_right;
+uint16_t distance_covered, distance_remaining;
+uint16_t deg_0, deg_20, deg_40, deg_60, deg_80, deg_neg20, deg_neg40, deg_neg60, deg_neg80;
+uint16_t dest_lat_val, dest_long_val, current_lat_val, current_long_val;
 
 float adc_read(void)
 {
@@ -70,7 +78,11 @@ float adc_read(void)
 bool period_init(void)
 {
     Uart2& u2 = Uart2::getInstance();
-    u2.init(38400);
+    u2.init(57600);
+
+    CAN_init(can1, 100, 10, 10, NULL, NULL);
+    CAN_reset_bus(can1);
+    CAN_bypass_filter_accept_all_msgs();
     return true; // Must return true upon success
 }
 
@@ -102,7 +114,7 @@ void period_1Hz(uint32_t count)
 
     char form = check_form();
 
-    if (form == 0)
+    if (form == Main_page)
     {
         int value = get_random_int(20);
         char random_speed = value;
@@ -112,16 +124,34 @@ void period_1Hz(uint32_t count)
     }
 
     //Frame 2
-    else if (form == 2)
+    else if (form == Distance_page)
     {
-    //Display LCD Numbers
-    display_lcd_numbers(Miles_covered, 1234);
-    display_lcd_numbers(Miles_remaining, 5678);
+        //Display LCD Numbers
+        display_lcd_numbers(Miles_covered, 1234);
+        display_lcd_numbers(Miles_remaining, 5678);
+    }
+
+    //Frame 1
+    else if (form == Sensor_page)
+    {
+        //Display Ultrasound Sensor readings
+        display_lcd_bar(Ultrasound_left, us_left*15);
+        display_lcd_bar(Ultrasound_front, us_front*15);
+        display_lcd_bar(Ultrasound_right, us_right*15);
+
+        display_LCD_large_led(Degree0, deg_0);
+        display_LCD_large_led(Degree20, deg_20);
+        display_LCD_large_led(Degree40, deg_40);
+        display_LCD_large_led(Degree60, deg_60);
+        display_LCD_large_led(Degree80, deg_80);
+        display_LCD_large_led(Degree_neg20, deg_neg20);
+        display_LCD_large_led(Degree_neg40, deg_neg40);
+        display_LCD_large_led(Degree_neg60, deg_neg60);
+        display_LCD_large_led(Degree_neg80, deg_neg80);
     }
 
     //Frame 3
-    else if (form == 3)
-    {
+    else if (form == GPS_page) {
 
         display_lcd_numbers(Dest_lat, 9101);
         display_lcd_numbers(Dest_long, 2345);
@@ -129,50 +159,96 @@ void period_1Hz(uint32_t count)
         display_lcd_numbers(Current_long, 65535);
     }
 
-    //Frame 1
-    else if (form == 1)
-    {
-        //Display Ultrasound Sensor readings
-        display_lcd_bar(Ultrasound_left, 50);
-        display_lcd_bar(Ultrasound_front, 25);
-        display_lcd_bar(Ultrasound_right, 10);
-
-        //Display Lidar
-        static int i;
-        if (i + 1 == 2)
-        {
-            display_LCD_large_led(10, On);
-            display_LCD_large_led(2, Off);
-        }
-
-        else
-        {
-            display_LCD_large_led(i - 1, On);
-            display_LCD_large_led(i, Off);
-        }
-        i++;
-        if (i == 11)
-        {
-            i = 2;
-        }
-
-    }
-
 }
+
+/**
+ * Do not require MIA messages for LCD
+ */
+const uint32_t                             SENSOR_DATA__MIA_MS = 3000;
+const SENSOR_DATA_t                        SENSOR_DATA__MIA_MSG = {0};
+const uint32_t                             HEARTBEAT__MIA_MS = 3000;
+const CAR_CONTROL_t                        CAR_CONTROL__MIA_MSG = {0};
+const uint32_t                             GPS_DATA__MIA_MS = 3000;
+const GPS_DATA_t                           GPS_DATA__MIA_MSG = {0};
+const uint32_t                             COMPASS__MIA_MS = 3000;
+const COMPASS_t                            COMPASS__MIA_MSG = {0};
 
 void period_10Hz(uint32_t count)
 {
+    static uint32_t counter;
 
+    SENSOR_DATA_t sensor_can_msg = { 0 };
+    COMPASS_t compass_can_msg = { 0 };
+    GPS_DATA_t gps_can_msg = { 0 };
+    CAR_CONTROL_t car_control_can_msg = { 0 };
+
+    can_msg_t can_msg;
+    counter++;
+
+    /**
+     * Counter to make sure that the values for LED are updated only once a second
+     */
+
+    // Empty all of the queued, and received messages within the last 10ms (100Hz callback frequency)
+    while (CAN_rx(can1, &can_msg, 0)) {
+        // Form the message header from the metadata of the arriving message
+        dbc_msg_hdr_t can_msg_hdr;
+        can_msg_hdr.dlc = can_msg.frame_fields.data_len;
+        can_msg_hdr.mid = can_msg.msg_id;
+
+        // Attempt to decode the message (brute force, but should use switch/case with MID)
+        dbc_decode_SENSOR_DATA(&sensor_can_msg, can_msg.data.bytes, &can_msg_hdr);
+        dbc_decode_GPS_DATA(&gps_can_msg, can_msg.data.bytes, &can_msg_hdr);
+        dbc_decode_COMPASS(&compass_can_msg, can_msg.data.bytes, &can_msg_hdr);
+        dbc_decode_CAR_CONTROL(&car_control_can_msg, can_msg.data.bytes, &can_msg_hdr);
+
+        //Update values from CAN every 10 iterations (1 Second)
+        if (counter % 10 == 0) {
+            //Update Lidar values
+            deg_0 = sensor_can_msg.LIDAR_0;
+            deg_20 = sensor_can_msg.LIDAR_20;
+            deg_40 = sensor_can_msg.LIDAR_40;
+            deg_60 = sensor_can_msg.LIDAR_60;
+            deg_80 = sensor_can_msg.LIDAR_80;
+            deg_neg20 = sensor_can_msg.LIDAR_neg20;
+            deg_neg40 = sensor_can_msg.LIDAR_neg40;
+            deg_neg60 = sensor_can_msg.LIDAR_neg60;
+            deg_neg80 = sensor_can_msg.LIDAR_neg80;
+
+            //Sonar values -- Feeding it now to the Ultrasound sensor values for LCD display
+            us_left = sensor_can_msg.SONAR_left;
+            us_right = sensor_can_msg.SONAR_right;
+            us_front = sensor_can_msg.SONAR_back;
+
+            //US values
+            current_lat_val = gps_can_msg.GPS_LATITUDE;
+            current_long_val = gps_can_msg.GPS_LONGITUDE;
+
+            //TODO - Need to update the speed value, distance remaining, distance covered, destination latitude and destination longitude
+
+            /**
+             * dest_lat_val = ;
+             * dest_long_val = ;
+             * speed = ;
+             * distance_covered = ;
+             * distance_remaining = ;
+             */
+
+
+
+
+
+        }
+    }
+    dbc_handle_mia_SENSOR_DATA(&sensor_can_msg, 10);
 }
 
 void period_100Hz(uint32_t count)
 {
-//    LE.toggle(3);
 }
 
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
 // scheduler_add_task(new periodicSchedulerTask(run_1Khz = true));
 void period_1000Hz(uint32_t count)
 {
-//    LE.toggle(4);
 }
