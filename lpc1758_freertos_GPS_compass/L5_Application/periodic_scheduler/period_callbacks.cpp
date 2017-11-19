4fd79797684622f17f16fc234ac942918f60e608
adeshmuk <deshmukhaditya404@gmail.com>
2017-11-19 15:46:22 -0800

    Compass heading angle 16-bit 0-359.9

--- lpc1758_freertos_GPS_compass/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ lpc1758_freertos_GPS_compass/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -34,7 +34,9 @@
 #include "io.hpp"
 //#include <iostream>
 #include "stdio.h"
-
+#include "can.h"
+#include "_can_dbc/generated_can.h"
+#include "string.h"
 
 
 /// This is the stack size used for each of the period tasks (1Hz, 10Hz, 100Hz, and 1000Hz)
@@ -48,9 +50,27 @@ const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
  */
 const uint32_t PERIOD_MONITOR_TASK_STACK_SIZE_BYTES = (512 * 3);
 
+can_t canTest = can1;
+
+COMPASS_t compass_msg = {0};
+uint8_t buffer[2] = { 0 };
+
+
+bool dbc_app_send_can_msg(uint32_t mid, uint8_t dlc, uint8_t bytes[8])
+{
+    can_msg_t can_msg = { 0 };
+    can_msg.msg_id = mid;
+    can_msg.frame_fields.data_len = dlc;
+    memcpy(can_msg.data.bytes, bytes, dlc);
+    return CAN_tx(canTest, &can_msg, 0);
+}
+
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void)
 {
+    CAN_init(canTest, 100, 10, 10, NULL, NULL);
+    CAN_bypass_filter_accept_all_msgs();
+    CAN_reset_bus(canTest);
     return true; // Must return true upon success
 }
 
@@ -70,15 +90,27 @@ bool period_reg_tlm(void)
 void period_1Hz(uint32_t count)
 {
     //LE.toggle(1);
-    uint8_t buffer[256] = { 0 };
     int addr = 0xc0;
-    int reg = 0x01;
-    int num_val_to_read = 1;
+    int reg = 0x02;
+    int bearing_int = 0;
+    float bearing_float = 0;
 
-    bool ok = I2C2::getInstance().readRegisters(addr, reg, &buffer[0], num_val_to_read);
-    printf("Read status from device %d: %s: \n", addr, ok ? "OK" : "ERROR");
-    for (unsigned int i = 0; i < num_val_to_read; i++) {
-        printf("    %#2X: %d\n", (reg + i), (buffer[i] & 0xFF));
+    if (I2C2::getInstance().readRegisters(addr, reg, &buffer[0], 2))
+    {
+        bearing_int = buffer[0];
+        bearing_int <<= 8;
+        bearing_int += buffer[1];
+        bearing_float = (float)((float)bearing_int/10.00);
+        //bearing_float = ceilf((bearing_float * 100) / 100);
+        printf("Int = %d\n", bearing_int);
+        printf("Float = %f\n\n", bearing_float);
+        compass_msg.CMP_BEARING = bearing_float;
+        dbc_encode_and_send_COMPASS(&compass_msg);
+    }
+
+    if(CAN_is_bus_off(can1))
+    {
+        CAN_reset_bus(can1);
     }
 }
 
