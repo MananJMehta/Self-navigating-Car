77db7028c6634cc01467ab53231f31507239bd37
Hugo <hugo.quiroz92@gmail.com>
2017-12-16 07:32:21 -0800

    Removed deprecated messages, turned off lidar distance values

--- lpc1758_freertos_Distance_sensors/L4_IO/src/sensor_functions.cpp
+++ lpc1758_freertos_Distance_sensors/L4_IO/src/sensor_functions.cpp
@@ -68,34 +68,34 @@ bool check_heartbeat()
 
 }
 
-///thinking about seperating sonar and lidar data
+/////thinking about seperating sonar and lidar data
+//void send_lidar_sonar_data()
+//{
+//    LIDAR_LANES_t SensorData;
+//
+//    SensorData.LIDAR_neg80 = rplidar.lane_lut[0];
+//    SensorData.LIDAR_neg60 = rplidar.lane_lut[1];
+//    SensorData.LIDAR_neg40 = rplidar.lane_lut[2];
+//    SensorData.LIDAR_neg20 = rplidar.lane_lut[3];
+//    SensorData.LIDAR_0 = rplidar.lane_lut[4];
+//    SensorData.LIDAR_20 = rplidar.lane_lut[5];
+//    SensorData.LIDAR_40 = rplidar.lane_lut[6];
+//    SensorData.LIDAR_60 = rplidar.lane_lut[7];
+//    SensorData.LIDAR_80 = rplidar.lane_lut[8];
+//    SensorData.SONAR_left = sonar.leftDist;
+//    SensorData.SONAR_right = sonar.rightDist;
+//    SensorData.SONAR_back = sonar.backDist;
+//
+//    //why are we printing?? Remove this if its not needed
+//    if(sync_flag)
+//    {
+//        if(dbc_encode_and_send_LIDAR_LANES(&SensorData))
+//            LE.toggle(4);
+//    }
+//
+//}
+
 void send_lidar_sonar_data()
-{
-    LIDAR_LANES_t SensorData;
-
-    SensorData.LIDAR_neg80 = rplidar.lane_lut[0];
-    SensorData.LIDAR_neg60 = rplidar.lane_lut[1];
-    SensorData.LIDAR_neg40 = rplidar.lane_lut[2];
-    SensorData.LIDAR_neg20 = rplidar.lane_lut[3];
-    SensorData.LIDAR_0 = rplidar.lane_lut[4];
-    SensorData.LIDAR_20 = rplidar.lane_lut[5];
-    SensorData.LIDAR_40 = rplidar.lane_lut[6];
-    SensorData.LIDAR_60 = rplidar.lane_lut[7];
-    SensorData.LIDAR_80 = rplidar.lane_lut[8];
-    SensorData.SONAR_left = sonar.leftDist;
-    SensorData.SONAR_right = sonar.rightDist;
-    SensorData.SONAR_back = sonar.backDist;
-
-    //why are we printing?? Remove this if its not needed
-    if(sync_flag)
-    {
-        if(dbc_encode_and_send_LIDAR_LANES(&SensorData))
-            LE.toggle(4);
-    }
-
-}
-
-void send_lidar_lane_data()
 {
     SENSOR_DATA_t LaneData;
 
@@ -126,56 +126,56 @@ void send_lidar_lane_data()
 }
 
 
-//this was a test and has now benn deprecated, remove at yor earliest convenience
-void send_distance_values()
-{
-    SENSOR_VALUES_t SensorValue;
+////this was a test and has now benn deprecated, remove at yor earliest convenience
+//void send_distance_values()
+//{
+//    SENSOR_VALUES_t SensorValue;
+//
+//    SensorValue.LIDAR_DISTANCE_CM = rplidar.distance_value_cm;
+//    SensorValue.LIDAR_ANGLE_DEG = rplidar.angle_value_deg;
+//    SensorValue.LIDAR_QUALITY_DEG = rplidar.quality_value;
+//    //add the Sonar Sensor code here
+//    if(sync_flag)
+//    {
+//        dbc_encode_and_send_SENSOR_VALUES(&SensorValue);
+//    }
+//
+//}
 
-    SensorValue.LIDAR_DISTANCE_CM = rplidar.distance_value_cm;
-    SensorValue.LIDAR_ANGLE_DEG = rplidar.angle_value_deg;
-    SensorValue.LIDAR_QUALITY_DEG = rplidar.quality_value;
-    //add the Sonar Sensor code here
-    if(sync_flag)
-    {
-        dbc_encode_and_send_SENSOR_VALUES(&SensorValue);
-    }
-
-}
-
-
-//this is deprecated
-void add_some_data_to_msg(LIDAR_DATA_VALUES_t *from)
-{
-    static uint8_t count = 0;
-
-    if (count == 0)
-    {
-        from->LIDAR_DISTANCE_CM_1 = rplidar.distance_value_cm;
-        from->LIDAR_ANGLE_DEG_1 = rplidar.angle_value_deg;
-        count++;
-    }
-    else if (count == 1)
-    {
-        from->LIDAR_DISTANCE_CM_2 = rplidar.distance_value_cm;
-        from->LIDAR_ANGLE_DEG_2 = rplidar.angle_value_deg;
-        count++;
-    }
-    else if (count == 2)
-    {
-        from->LIDAR_DISTANCE_CM_3 = rplidar.distance_value_cm;
-        from->LIDAR_ANGLE_DEG_3 = rplidar.angle_value_deg;
-        count = 0;
-    }
-}
 
 //this is deprecated
-void send_three_values(LIDAR_DATA_VALUES_t *from)
-{
-    if(sync_flag && !noop_flag)
-    {
-        dbc_encode_and_send_LIDAR_DATA_VALUES(from);
-    }
-}
+//void add_some_data_to_msg(LIDAR_DATA_VALUES_t *from)
+//{
+//    static uint8_t count = 0;
+//
+//    if (count == 0)
+//    {
+//        from->LIDAR_DISTANCE_CM_1 = rplidar.distance_value_cm;
+//        from->LIDAR_ANGLE_DEG_1 = rplidar.angle_value_deg;
+//        count++;
+//    }
+//    else if (count == 1)
+//    {
+//        from->LIDAR_DISTANCE_CM_2 = rplidar.distance_value_cm;
+//        from->LIDAR_ANGLE_DEG_2 = rplidar.angle_value_deg;
+//        count++;
+//    }
+//    else if (count == 2)
+//    {
+//        from->LIDAR_DISTANCE_CM_3 = rplidar.distance_value_cm;
+//        from->LIDAR_ANGLE_DEG_3 = rplidar.angle_value_deg;
+//        count = 0;
+//    }
+//}
+
+////this is deprecated
+//void send_three_values(LIDAR_DATA_VALUES_t *from)
+//{
+//    if(sync_flag && !noop_flag)
+//    {
+//        dbc_encode_and_send_LIDAR_DATA_VALUES(from);
+//    }
+//}
 
 
 void send_lane_distance_values ()
